696190d1d2f2890082b5da71db133b466c05bbe7
svawek <svawek@gmail.com>
2018-09-13 00:01:36 +0300

    Lesson 1 update

--- lesson_1/discriminant.rb
+++ lesson_1/discriminant.rb
@@ -8,14 +8,15 @@ puts "Введите клэфициент c"
 c = gets.chomp.to_f
 
 discriminant = b**2 - (4 * a * c)
+discriminant_sqrt = Math.sqrt(discriminant)
 
 if discriminant > 0
-    x_1 = (-b + Math.sqrt(discriminant)) / (2 * a)
-    x_2 = (-b - Math.sqrt(discriminant)) / (2 * a)
-    puts "Дискриминант = #{discriminant}, x1 = #{x_1}, x2 = #{x_2}"
+  x_1 = (-b + discriminant_sqrt) / (2 * a)
+  x_2 = (-b - discriminant_sqrt) / (2 * a)
+  puts "Дискриминант = #{discriminant}, x1 = #{x_1}, x2 = #{x_2}"
 elsif discriminant == 0
-    x = (-b) / (2 * a)
-    puts "Дискриминант = #{discriminant}, x1, x2 = #{x}"
+  x = (-b) / (2 * a)
+  puts "Дискриминант = #{discriminant}, x1, x2 = #{x}"
 else
-    puts "Корней нет"
+  puts "Корней нет"
 end
--- lesson_1/right_triangle.rb
+++ lesson_1/right_triangle.rb
@@ -7,24 +7,15 @@ b = gets.chomp.to_f
 puts "Укажите длину третьей стороны треугольника"
 c = gets.chomp.to_f
 
-if a > b && a > c
-  hypotenuse = a
-  side_1 = b
-  side_2 = c
-elsif b > a && b > c
-  hypotenuse = b
-  side_1 = a
-  side_2 = c
-else
-  hypotenuse = c
-  side_1 = a
-  side_2 = b
-end
+triangle = [a, b, c]
+triangle.sort!
 
-if hypotenuse**2 == side_1**2 + side_2**2 && side_1 == side_2
+if triangle[2]**2 == triangle[0]**2 + triangle[1]**2 && triangle[0] == triangle[1]
   puts "Треугольник прямоугольный и равнобедренный"
-elsif hypotenuse**2 == side_1**2 + side_2**2
+elsif triangle[2]**2 == triangle[0]**2 + triangle[1]**2
   puts "Треугольник прямоугольный"
+elsif triangle[0] == triangle[1] && triangle[0] != triangle[2]
+  puts "Треугольник равнобедренный, но не прямоугольный"
 elsif a == b && b == c
   puts "Треугольник равнобедренный и равносторонний, но не прямоугольный"
 else
